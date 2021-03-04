09d4586b34b93ce856b348efd4d8b53ef4ddbae9
Vasilis Charalampidis <39488070+MusicalJunkyart@users.noreply.github.com>
2021-03-05 01:10:40 +0200

    Delete @model directory

--- @model/activateNeurons.m
+++
@@ -1,8 +0,0 @@
-function detectorSignal = activateNeurons(thisModel, theta)
-%STIMULATENEURONS
-%
-    activationProbability = thisModel.hyperParameters.activationProbability;
-    isInRange = stimulateNeurons(thisModel, theta);
-    isActivated = (rand(size(isInRange)) < activationProbability);
-    detectorSignal = (isInRange & isActivated);
-end
